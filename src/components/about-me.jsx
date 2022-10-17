0ba65e2ef61a77805e4c95dbd02057849560b2c8
ImJars <ra374885@uaeh.edu.mx>
2022-10-16 23:59:21 -0500

    Add about-me-section

---
+++ src/components/about-me.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const AboutMe = () => {
+    return ( 
+        <>
+            <section
+                className="w-full h-screen bg-primary"
+            >
+                <div>
+                    <h1>About Me</h1>
+                </div>
+            </section>
+        </>
+     );
+}
+ 
+export default AboutMe;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,14 @@
 import * as React from "react"
+import AboutMe from "../components/about-me"
 import Home from "../components/home"
 // import Layout from "../components/layout"
 import Seo from "../components/seo"
 
 const IndexPage = () => (
-    <Home />
+    <>
+        <Home />
+        <AboutMe />
+    </>
 )
 
 /**
