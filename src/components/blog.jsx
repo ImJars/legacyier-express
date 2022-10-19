be64f8175b60ba5323ffeae120c3b727a2718832
ImJars <ra374885@uaeh.edu.mx>
2022-10-18 21:46:27 -0500

    Add blog-section-concept

--- src/components/blog.jsx
+++ src/components/blog.jsx
@@ -1,9 +1,80 @@
 import React from 'react';
+import BlogCard from './components/blog-card';
+import Title from './components/title';
+import Img01 from '../images/designer-01.jpg';
+import Img02 from '../images/designer-02.jpg';
+import Img03 from '../images/designer-03.jpg';
+import Img06 from '../images/designer-06.jpg';
+
 
 const Blog = () => {
     return ( 
         <>
-            
+            <section
+                className="w-full h-sfull bg-primary text-white
+                font-sans flex justify-center items-center py-40"
+            >   
+                <div
+                    className='max-w-250 flex flex-col'
+                >
+                    <Title 
+                        title={'My Blog'}
+                        subtitle={'The last posts about graphic design and web design'}
+                    />
+                    <div
+                        className='grid grid-cols-2 gap-10 mt-14'
+                    >
+                        <BlogCard 
+                            img={ Img01 }
+                            date={'09 March 2020'}
+                            hastag01= { '#design' }
+                            hastag02={ '#web' }
+                            hastag03={ '#graphic' }
+                            title={'Whats the difference between graphic design and web design?'}
+                            description={
+                                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+                            }
+                            link={'https://github.com/ImJars'}
+                        />
+                        <BlogCard 
+                            img={ Img02 }
+                            date={'09 March 2020'}
+                            hastag01= { '#design' }
+                            hastag02={ '#web' }
+                            hastag03={ '#graphic' }
+                            title={'How to create successful website for yout business and brand?'}
+                            description={
+                                'lorem ipsum dolor sit amet, consectetur adipiscing elit. lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+                            }
+                            link={'https://github.com/ImJars'}
+                        />
+                        <BlogCard 
+                            img={ Img03 }
+                            date={'09 March 2020'}
+                            hastag01= { '#design' }
+                            hastag02={ '#web' }
+                            hastag03={ '#graphic' }
+                            title={'How to create successful website for yout business and brand?'}
+                            description={
+                                'lorem ipsum dolor sit amet, consectetur adipiscing elit. lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+                            }
+                            link={'https://github.com/ImJars'}
+                        />
+                        <BlogCard 
+                            img={ Img06 }
+                            date={'09 March 2020'}
+                            hastag01= { '#design' }
+                            hastag02={ '#web' }
+                            hastag03={ '#graphic' }
+                            title={'How to create successful website for yout business and brand?'}
+                            description={
+                                'lorem ipsum dolor sit amet, consectetur adipiscing elit. lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+                            }
+                            link={'https://github.com/ImJars'}
+                        />
+                    </div>
+                </div>
+            </section>
         </>
      );
 }
---
+++ src/components/components/blog-card.jsx
@@ -0,0 +1,78 @@
+import React from 'react'
+
+const BlogCard = ({ 
+    img, 
+    date, 
+    hastag01, 
+    hastag02, 
+    hastag03,
+    title,
+    description,
+    link,
+}) => {
+    return ( 
+        <>
+            <div
+                className='static font-sans pb-40'
+            >
+                <div
+                    className='relative'
+                >
+                    <img 
+                        src={ img } 
+                        alt="blog" 
+                        className='rounded-sm'
+                    />
+                    <div
+                        className='absolute rounded-2xl bg-secondary mx-4 px-6 py-6
+                        -bottom-44 left-0'
+                    >
+                        <div
+                            className='flex space-x-2 text-xs 
+                                    text-text-mini tracking-wide'
+                        >
+                            <h3
+                                className='border-l-2 border-tertiary pl-2'
+                            >
+                                { date }
+                            </h3>
+                            <font>
+                                &#8226;
+                            </font>
+                            <h2>
+                                { hastag01}
+                            </h2>
+                            <h2>
+                                { hastag02 }
+                            </h2>
+                            <h2>
+                                { hastag03 }
+                            </h2>
+                        </div>
+                        <h1
+                            className='text-base font-semibold mt-2'
+                        >
+                            { title }
+                        </h1>
+                        <p
+                            className='text-xs mt-2 text-text-mini'
+                        >
+                            { description }
+                        </p>
+                        <div>
+                            <a
+                                className='rounded-md bg-tertiary text-sm
+                                        text-white px-4 py-2 mt-4 inline-block' 
+                                href={ link }
+                            >
+                                Read more &#8594;
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+     );
+}
+ 
+export default BlogCard;
