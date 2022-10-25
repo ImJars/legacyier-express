2afbf2832b4b9e8bada9d25841c12485cd38ea2b
ImJars <ra374885@uaeh.edu.mx>
2022-10-24 22:25:32 -0500

    fixing-home

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -4,100 +4,125 @@ import { FaFacebookF, FaTwitter, FaInstagram, FaLinkedinIn, FaGithub } from 'rea
 const Home = ({ refHome }) => {
     return ( 
         <>
-            <div
+            <section
                 ref={ refHome}
                 id='Home'
-                className='w-full h-screen bg-secondary text-white'
+                className='w-full h-screen bg-secondary text-white
+                        font-sans flex justify-center items-center'
             >
-                <div 
-                    className='w-1/2 flex flex-col justify-between h-full'
+                <div
+                    className='max-w-250 h-screen flex'
                 >
                     <div
-                        className='flex space-x-4 mt-8 justify-center'
+                        className='w-1/2 grid grid-cols-1 content-between my-8'
                     >
-                        <a href="https://github.com/ImJars">
-                            <FaFacebookF />
-                        </a>
-                        <a href="https://github.com/ImJars">
-                            <FaTwitter />
-                        </a>
-                        <a href="https://github.com/ImJars">
-                            <FaInstagram />
-                        </a>
-                        <a href="https://github.com/ImJars">
-                            <FaLinkedinIn />
-                        </a>
-                        <a href="https://github.com/ImJars">
-                            <FaGithub />
-                        </a>
-                    </div>
-                    <div
-                        className='text-white font-sans grid content-center justify-end'
-                    >
+                        <div
+                            className='flex justify-start items-center space-x-5'
+                        >
+                            <a href="https://twitter.com/Im_Jars">
+                                <FaFacebookF 
+                                    className='text-lg hover:text-septenary
+                                            transition duration-500 ease-in-out'
+                                />
+                            </a>
+                            <a href="https://twitter.com/Im_Jars">
+                                <FaTwitter 
+                                    className='text-lg hover:text-septenary
+                                            transition duration-500 ease-in-out'
+                                />
+                            </a>
+                            <a href="https://twitter.com/Im_Jars">
+                                <FaInstagram 
+                                    className='text-lg hover:text-septenary
+                                            transition duration-500 ease-in-out'
+                                />
+                            </a>
+                            <a href="https://twitter.com/Im_Jars">
+                                <FaLinkedinIn 
+                                    className='text-lg hover:text-septenary
+                                            transition duration-500 ease-in-out'
+                                />
+                            </a>
+                            <a href="https://twitter.com/Im_Jars">
+                                <FaGithub 
+                                    className='text-lg hover:text-septenary
+                                            transition duration-500 ease-in-out'
+                                />
+                            </a>
+                        </div>
                         <div>
                             <h1
-                                className='text-sm'
+                                className='text-xs tracking-widest'
                             >
-                                Hi There,
+                                Hi everyone,
                             </h1>
-                        </div>
-                        <div>
                             <h2
-                                className='text-3xl font-bold'
+                                className='text-4xl font-bold'
                             >
-                                I Am a Developer
+                                I'm Software Developer
                             </h2>
-                        </div>
-                        <div>
-                            <h3>
-                                Lorem ipsum dolor sit amet consectetur Lorem, ipsum dolor sit <br/> amet 
-                                consectetur adipisicing elit. Possimus
-                            </h3>
-                        </div>
-                        <div>
-                            <button
-                                className='border-2 border-septenary 
-                                    px-4 py-2 mt-4 rounded-md bg-septenary
-                                    hover:bg-secondary hover:transition-all 
-                                    hover:duration-500'
+                            <h3
+                                className='text-lg text-septenary'
                             >
-                                More About Me
-                            </button>
+                                I build things for the web.
+                            </h3>
+                            <p
+                                className='text-sm mt-2 text-text-mini tracking-wide'
+                            >
+                                I'm a software developer specializing in creating exceptional 
+                                products and digital experiences. I am currently focused on 
+                                creating <font className='text-septenary'>Web3</font> technology products.
+                            </p>
                         </div>
-                    </div>
+                        <div
+                            className='flex space-x-10'
+                        >
+                            <div>
+                                <h1
+                                    className='text-sm font-semibold'
+                                >
+                                    Email
+                                </h1>
+                                <h2
+                                    className='text-xs text-text-mini tracking-widest mt-1'
+                                >
+                                    hi.legacyier@gmail.com
+                                </h2>
+                            </div>
+                            <div>
+                                <h1
+                                    className='text-sm font-semibold'
+                                >
+                                    WhatsApp
+                                </h1>
+                                <h2
+                                    className='text-xs text-text-mini tracking-widest mt-1'
+                                >
+                                    +52 773-151-1761
+                                </h2>
+                            </div>
+                            <div>
+                                <h1
+                                    className='text-sm font-semibold'
+                                >
+                                    Location
+                                </h1>
+                                <h2
+                                    className='text-xs text-text-mini tracking-widest mt-1'
+                                >
+                                    Hidalgo, Mex
+                                </h2>
+                            </div>
+                        </div>
+                        </div>
                     <div
-                        className='flex space-x-8 mb-8 justify-end'
+                        className='w-1/2'
                     >
-                        <div>
-                            <h1>
-                                Email
-                            </h1>
-                            <p>
-                                hi.legacyier@gmail.com
-                            </p>
-                        </div>
-                        <div>
-                            <h1>
-                                Phone
-                            </h1>
-                            <p>
-                                +52 55 2914 7674
-                            </p>
-                        </div>
-                        <div>
-                            <h1>
-                                Location
-                            </h1>
-                            <p>
-                                Mexico City, Mexico
-                            </p>
-                        </div>
+                        <img src="#" alt="" />
                     </div>
                 </div>
-                <div>
-
-                </div>
-            </div>
+                
+            </section>
         </>
      );
 }
