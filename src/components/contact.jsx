e4677357c52e0b12b49c71fcd9a0a7538105e382
ImJars <ra374885@uaeh.edu.mx>
2022-11-23 20:48:59 -0600

    Add Contact-section

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -6,12 +6,13 @@ const Contact = ({ refContact }) => {
             <section
                 ref={ refContact }
                 id='Contact'
-                className="w-full h-screen bg-primary text-white font-sans"
+                className="static w-full h-screen bg-primary text-white font-sans"
             >
                 <div
-                    className="flex justify-center text-center items-center h-full"
+                    className="relative flex justify-center text-center items-center h-full"
                 >
-                    <div>
+                    <div
+                    >
                         <h1
                             className='text-5xl font-bold'
                         >
@@ -27,14 +28,38 @@ const Contact = ({ refContact }) => {
                         </h2>
                         <a href="mailto:hi.legacyier@gmail.com">
                             <button
-                               className='border bg-primary py-4 px-8 mt-5
-                                        rounded-md hover:bg-septenary hover:bg-opacity-25
-                                        hover:transition hover:duration-300 hover:ease-in-out' 
+                               className='border-septenary border text-septenary 
+                                      bg-primary py-4 px-8 mt-5 rounded-md hover:bg-septenary 
+                                        hover:bg-opacity-20 hover:transition hover:duration-300 
+                                        hover:ease-in-out' 
                             >
                                 Say Hello
                             </button>
                         </a>
                     </div>
+                    <footer
+                        className='absolute w-full h-14 bg-primary bottom-0'
+                    >   
+                        <div
+                            className='font-sans flex justify-center items-center h-full'
+                        >
+                            <a
+                                href="https://twitter.com/Im_Jars"
+                                target="_blank"
+                                rel="noreferrer"
+                                className='text-text-mini text-xs tracking-widest
+                                            hover:text-septenary hover:transition
+                                            hover:duration-500 hover:ease-in-out'
+                            >
+                                <h1>
+                                    Designed & Built by Angel Ramirez
+                                </h1>
+                                <h2>
+                                    © 2021 ImJars
+                                </h2>
+                            </a>
+                        </div>
+                    </footer>
                 </div>
             </section>
         </>
